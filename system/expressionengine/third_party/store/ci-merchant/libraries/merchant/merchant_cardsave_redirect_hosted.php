5d831ea31e8353f24d4d61fbc250ecba28f6b5be
Joe Dixon <joe@smokyjoe.co.uk>
2013-09-07 11:38:04 +0100

    -

--- system/expressionengine/third_party/store/ci-merchant/libraries/merchant/merchant_cardsave_redirect_hosted.php
+++ system/expressionengine/third_party/store/ci-merchant/libraries/merchant/merchant_cardsave_redirect_hosted.php
@@ -54,6 +54,11 @@ class Merchant_cardsave_redirect_hosted extends Merchant_driver
 
 	public function purchase_return()
 	{
+		if(!isset($_POST['StatusCode'])){
+			$response new Merchant_response('complete', 'Transaction Complete', '12345');
+			$response->_data = $_POST;
+			return $response;
+		}
 		$transauthorised = FALSE; 
 		switch (intval($this->CI->input->post('StatusCode')))
 		{
